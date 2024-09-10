94a74094486998c2dcad79594e87718144f2e4ad
Daniel Serna <danielsernaq@gmail.com>
2024-09-10 11:19:57 -0500

    feat:configure environment variables in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:catbreeds/config/router/app_router.dart';
-
 import 'package:catbreeds/config/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
